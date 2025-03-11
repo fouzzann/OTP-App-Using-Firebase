7f91bc0def5bba7c7cdcd9810621ac51b52fdaf1
fouzan. p <fouzanparammal2@gmail.com>
2025-03-11 13:33:37 +0530

    added the circular progress indicator on the web view page

--- lib/views/screens/web_view_screen.dart
+++ lib/views/screens/web_view_screen.dart
@@ -1,7 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
+import 'package:otp_using_firebase/views/screens/phone_auth.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-import 'package:get/get.dart';
 
 class WebViewPage extends StatefulWidget {
   const WebViewPage({Key? key}) : super(key: key);
@@ -12,240 +12,65 @@ class WebViewPage extends StatefulWidget {
 
 class _WebViewPageState extends State<WebViewPage> {
   late final WebViewController _controller;
-  bool isLoading = true;
-  double loadingProgress = 0.0;
-  String currentUrl = 'https://flutter.dev/showcase';
-  final TextEditingController _urlController = TextEditingController();
+  bool isLoading = true; // Track loading state
 
   @override
   void initState() {
     super.initState();
-    _urlController.text = currentUrl;
-    _initializeWebView();
-  }
-
-  void _initializeWebView() {
     _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setBackgroundColor(const Color(0xFFF8F9FA))
       ..setNavigationDelegate(
         NavigationDelegate(
           onPageStarted: (String url) {
             setState(() {
-              isLoading = true;
-              currentUrl = url;
-              _urlController.text = url;
-            });
-          },
-          onProgress: (int progress) {
-            setState(() {
-              loadingProgress = progress / 100;
+              isLoading = true; // Show loading indicator
             });
           },
           onPageFinished: (String url) {
             setState(() {
-              isLoading = false;
+              isLoading = false; // Hide loading indicator
             });
-          },
-          onNavigationRequest: (NavigationRequest request) {
-            return NavigationDecision.navigate;
           },
         ),
       )
-      ..loadRequest(Uri.parse(currentUrl));
+      ..loadRequest(Uri.parse('https://flutter.dev/showcase'));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       appBar: AppBar(
-        elevation: 0,
+        automaticallyImplyLeading: false,
         backgroundColor: Colors.white,
         title: const Text(
           "Web Browser",
-          style: TextStyle(
-            color: Colors.black87,
-            fontWeight: FontWeight.w600,
-          ),
-        ),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.black87),
-          onPressed: () async {
-            if (await _controller.canGoBack()) {
-              _controller.goBack();
-            } else {
-              Navigator.pop(context);
-            }
-          },
+          style: TextStyle(fontWeight: FontWeight.w600),
         ),
         actions: [
           IconButton(
-            icon: const Icon(Icons.logout, color: Colors.black87),
             onPressed: () {
-              showDialog(
-                context: context,
-                builder: (context) => AlertDialog(
-                  title: const Text('Sign Out'),
-                  content: const Text('Are you sure you want to sign out?'),
-                  actions: [
-                    TextButton(
-                      onPressed: () => Navigator.of(context).pop(),
-                      child: const Text('CANCEL'),
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        FirebaseAuth.instance.signOut();
-                        Navigator.of(context).pop();
-                        Get.offAllNamed('/'); // Navigate to login page
-                      },
-                      child: const Text('SIGN OUT'),
-                    ),
-                  ],
-                ),
-              );
+              Get.to(() => PhoneAuth());
             },
+            icon: const Icon(Icons.logout),
           ),
           IconButton(
-            icon: const Icon(Icons.refresh, color: Colors.black87),
-            onPressed: () => _controller.reload(),
+            icon: const Icon(Icons.refresh, color: Colors.black),
+            onPressed: () {
+              _controller.reload();
+            },
           ),
         ],
       ),
-      body: Column(
+      body: Stack(
         children: [
-          // URL Bar
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.05),
-                  blurRadius: 4,
-                  offset: const Offset(0, 2),
-                ),
-              ],
+          WebViewWidget(controller: _controller),
+          if (isLoading) 
+            const Center(
+              child: CircularProgressIndicator(color: Colors.blue),
             ),
-            child: Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    height: 40,
-                    decoration: BoxDecoration(
-                      color: Colors.grey.shade100,
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: TextField(
-                      controller: _urlController,
-                      decoration: const InputDecoration(
-                        hintText: 'Enter URL',
-                        border: InputBorder.none,
-                        contentPadding: EdgeInsets.only(bottom: 10),
-                      ),
-                      onSubmitted: (value) {
-                        if (value.isNotEmpty) {
-                          final url = _ensureHttps(value);
-                          _controller.loadRequest(Uri.parse(url));
-                        }
-                      },
-                    ),
-                  ),
-                ),
-                IconButton(
-                  icon: const Icon(Icons.search, color: Colors.blue),
-                  onPressed: () {
-                    final url = _ensureHttps(_urlController.text);
-                    _controller.loadRequest(Uri.parse(url));
-                  },
-                ),
-              ],
-            ),
-          ),
-          
-          // Progress Bar
-          if (isLoading)
-            LinearProgressIndicator(
-              value: loadingProgress > 0 ? loadingProgress : null,
-              backgroundColor: Colors.grey.shade200,
-              valueColor: const AlwaysStoppedAnimation<Color>(Colors.blue),
-              minHeight: 3,
-            ),
-            
-          // WebView
-          Expanded(
-            child: Stack(
-              children: [
-                WebViewWidget(controller: _controller),
-                if (isLoading && loadingProgress == 0)
-                  const Center(
-                    child: CircularProgressIndicator(),
-                  ),
-              ],
-            ),
-          ),
         ],
-      ),
-      bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.05),
-              blurRadius: 4,
-              offset: const Offset(0, -2),
-            ),
-          ],
-        ),
-        height: 60,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            IconButton(
-              icon: const Icon(Icons.arrow_back_ios, color: Colors.black54),
-              onPressed: () async {
-                if (await _controller.canGoBack()) {
-                  _controller.goBack();
-                }
-              },
-            ),
-            IconButton(
-              icon: const Icon(Icons.arrow_forward_ios, color: Colors.black54),
-              onPressed: () async {
-                if (await _controller.canGoForward()) {
-                  _controller.goForward();
-                }
-              },
-            ),
-            IconButton(
-              icon: const Icon(Icons.home, color: Colors.black54),
-              onPressed: () {
-                _controller.loadRequest(Uri.parse('https://flutter.dev/showcase'));
-              },
-            ),
-            IconButton(
-              icon: const Icon(Icons.bookmark_border, color: Colors.black54),
-              onPressed: () {
-                // Implement bookmark functionality
-              },
-            ),
-          ],
-        ),
       ),
     );
   }
-
-  String _ensureHttps(String url) {
-    if (!url.startsWith('http://') && !url.startsWith('https://')) {
-      return 'https://$url';
-    }
-    return url;
-  }
-
-  @override
-  void dispose() {
-    _urlController.dispose();
-    super.dispose();
-  }
 }
+ 
