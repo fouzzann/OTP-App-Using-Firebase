e905e48773c5263c61f96515a5a15420648ce242
fouzan. p <fouzanparammal2@gmail.com>
2025-06-30 20:21:58 +0530

    fixed to nuewmerical only

--- lib/views/widgets/phone_auth_screen/phone_input_field.dart
+++ lib/views/widgets/phone_auth_screen/phone_input_field.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:intl_phone_field/intl_phone_field.dart';
 
 class PhoneInputField extends StatelessWidget {
   final Function(String) onChanged;
   final String initialCountryCode;
-  
+
   const PhoneInputField({
     super.key,
     required this.onChanged,
@@ -23,10 +24,12 @@ class PhoneInputField extends StatelessWidget {
         fillColor: Colors.grey.withOpacity(0.1),
         contentPadding: const EdgeInsets.symmetric(
           horizontal: 16,
-          vertical: 16,
+          vertical: 16, 
         ),
       ),
       initialCountryCode: initialCountryCode,
+      keyboardType: TextInputType.number, // Ensure numeric keyboard
+      inputFormatters: [FilteringTextInputFormatter.digitsOnly], // Restrict to digits
       onChanged: (phone) {
         onChanged(phone.completeNumber);
       },
